73ae9d3fb196a64272ebd4181fa53dcb2a449e23
Rafael Jeferson <rafa.jefer@gmail.com>
2019-05-24 17:00:12 -0300

    Listando os dados na tela com ajax

--- tasks-app/app.js
+++ tasks-app/app.js
@@ -1,7 +1,8 @@
 $(function () {
-   console.log('Hello World');
+   // console.log('Hello World');
    $('#task-result').hide();
-
+   fetchTasks();
+   // Search on table
    $('#search').keyup(function () {
       if ($('#search').val()) {
          let search = $('#search').val();
@@ -23,18 +24,46 @@ $(function () {
          });
       }
    });
-   
-   $('#task-form').submit(function(e) {
+
+   // Insert on table
+   $('#task-form').submit(function (e) {
       e.preventDefault();
-      
+
       const postData = {
          name: $('#name').val(),
          description: $('#description').val()
       };
-      $.post('task-add.php', postData, function(response) {
-         console.log(response);
-         
+      $.post('task-add.php', postData, function (response) {
+         fetchTasks();
+
          $('#task-form').trigger('reset');
       });
    });
+
+   function fetchTasks() {
+      // Select on table
+      $.ajax({
+         url: 'task-list.php',
+         type: 'GET',
+         success: function (response) {
+            let tasks = JSON.parse(response);
+            let template = '';
+            tasks.forEach(task => {
+               template += `
+               <tr>
+                  <td>${task.id}</td>
+                  <td>${task.name}</td>
+                  <td>${task.description}</td>
+                  <td>
+                     <button class="btn btn-danger">Delete</button>
+                  </td>
+               </tr>
+               `
+            });
+            console.log(tasks);
+            $('#tasks').html(template);
+         }
+      });
+   }
+
 });
